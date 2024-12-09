d0d80d2ffc69c8b6cff0ffb283a8601d3952c03c
Saul <158204665+UO294936@users.noreply.github.com>
2024-12-09 16:27:48 +0100

    Add files via upload

---
+++ php/aplicacionPhp.php
@@ -0,0 +1,58 @@
+<?php
+    class Hola{
+        
+    }
+?>
+
+<!DOCTYPE HTML>
+    <html lang="es">
+    <head>
+        <!-- Datos que describen el documento -->
+        <meta charset="UTF-8" />
+        <title>Aplicación Web de la Fórmula 1</title>
+        <meta name="author" content="Saúl Martín Fernández"/>
+        <meta name="description" content="Aplicación Web de la Fórmula 1"/>
+        <meta name ="viewport" content ="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="../estilo/estilo.css" />
+        <link rel="stylesheet" type="text/css" href="../estilo/layout.css" />
+        <link rel="stylesheet" type="text/css" href="../estilo/semaforo_grid.css" />
+        <link rel="icon" href="../multimedia/imagenes/favicon_f1Desktop.ico" type="image/x-icon"/>
+    </head>
+
+    <body>
+        <header>
+            <h1><a href="../index.html" title="F1 Desktop">F1 Desktop</a></h1>
+        
+            <!-- Datos con el contenido que aparece en el navegador -->
+            <nav>
+                <a href="../index.html" title="Inicio">Inicio</a>
+                <a href="../piloto.html" title="Piloto">Piloto</a>
+                <a href="../noticias.html" title ="Noticias">Noticias</a>
+                <a href="../calendario.html" title="Calendario">Calendario</a>
+                <a href="../meteorologia.html" title="Meteorologia">Meteorologia</a>
+                <a href="../circuito.html" title="Circuito">Circuito</a>
+                <a href="../viajes.php" title="Viajes">Viajes</a>
+                <a href="../juegos.html" class="active" title="Juegos">Juegos</a>
+            </nav>
+        </header>
+
+        <p>
+            Estás en: 
+            <a href="../index.html" title="Inicio">Inicio</a> | <a href="../juegos.html" title="Juegos">Juegos</a> | Aplicación Php
+        </p>
+
+        <aside>
+            <nav>
+                <a href="../memoria.html" title="Memoria">Juego Memoria</a>
+                <a href="../semaforo.php" title="Semaforo">Juego Semáforo</a>
+                <a href="../api.html" title="Aplicación Web">Aplicación Web</a>
+                <a href="aplicacionPhp.php" title="Aplicación Php">Aplicación Php</a>
+            </nav>
+        </aside>
+
+        <main>  
+            <p>En desarollo</p>
+        </main>
+
+    </body>
+</html>
